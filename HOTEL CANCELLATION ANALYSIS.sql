632b80d49a9e3f088137d81e4696a09259845f32
manishDataOps <mandalmanish370@gmail.com>
2026-01-12 23:49:53 +0530

    Add files via upload

---
+++ HOTEL CANCELLATION ANALYSIS.sql
@@ -0,0 +1,82 @@
+-- HOTEL BOOKING CANCELLATION ANALYSIS
+
+--(1) TOTAL BOOKING CANCELLATION OVERVIEW
+
+	SELECT 
+	COUNT(*) AS total_booking ,
+	SUM(CASE WHEN is_canceled = 1 THEN 1 ELSE 0 END) AS total_cancellation,
+	CONCAT(CAST(SUM(CASE WHEN is_canceled = 1 THEN 1 ELSE 0 END) * 100 / COUNT(*) AS decimal(5,2)),'%') AS cancellation_rate
+	FROM hotel_booking ;
+
+--(2) CITY VS RESORT CANCELLATION OVERVIEW
+
+	SELECT
+	hotel,
+	COUNT(*) AS total_booking,
+	SUM(CASE WHEN is_canceled = 1 THEN 1 ELSE 0 END) AS  total_cancellation,
+	CONCAT(CAST(SUM(CASE WHEN is_canceled = 1 THEN 1 ELSE 0 END) * 100.0 / COUNT(*) AS decimal(5,2)),'%') AS cancellation_rate
+	FROM hotel_booking
+	GROUP BY hotel
+	ORDER BY total_cancellation DESC ;
+
+--(3) LEAD TIME WISE ANALYSIS
+	
+	SELECT
+	CASE
+		WHEN lead_time <= 30 THEN 'below 30 days'
+		WHEN lead_time BETWEEN 31 AND 60 THEN '31 to 60 days'
+		ELSE 'above 60 days' end AS lead_time_group,
+	COUNT(*) AS total_booking,
+		SUM(CASE WHEN is_canceled = 1 THEN 1 ELSE 0 END) AS total_cancellation,
+		CONCAT(CAST(SUM(CASE WHEN is_canceled = 1 THEN 1 ELSE 0 END) * 100.0 / COUNT(*) AS decimal(5,2)),'%') AS cancellation_rate
+	FROM hotel_booking
+	GROUP BY 
+		CASE
+		WHEN lead_time <= 30 THEN 'below 30 days'
+		WHEN lead_time BETWEEN 31 AND 60 THEN '31 to 60 days'
+		ELSE 'above 60 days' end
+		ORDER BY total_cancellation DESC ;
+
+--(4) HOTEL CANCELLATION OVERVIEW
+
+	SELECT
+	market_segment,
+	COUNT(*) AS total_booking,
+	SUM(CASE WHEN is_canceled = 1 THEN 1 ELSE 0 END) AS total_cancellation,
+	CONCAT(CAST(SUM(CASE WHEN is_canceled = 1 THEN 1 ELSE 0 END) * 100.0 / COUNT(*) AS decimal(5,2)),'%') AS cancellation_rate
+	FROM hotel_booking
+	GROUP BY market_segment
+	ORDER BY total_booking DESC, cancellation_rate DESC ;
+
+--(5) UNIQUE VS DUPLICATE
+	
+	SELECT
+	CASE WHEN is_repeated_guest = 1 THEN 'repeted_customer' ELSE 'unique_customer' END AS customer_type,
+	COUNT(*) AS total_booking,
+	SUM(CASE WHEN is_canceled = 1 THEN 1 ELSE 0 END) AS total_cancellation,
+	CONCAT(CAST(SUM(CASE WHEN is_canceled = 1 THEN 1 ELSE 0 END) * 100.0 / COUNT(*) AS decimal(5,2)),'%') AS cancellation_rate
+	FROM hotel_booking
+	GROUP BY CASE WHEN is_repeated_guest = 1 THEN 'repeted_customer' ELSE 'unique_customer' END ;
+
+--(6) YEAR WISE WISE CANCELLATION ANALYSIS
+
+	SELECT
+	FORMAT(reservation_status_date,'yyyy') AS year,
+	COUNT(*) AS total_booking,
+	SUM(CASE WHEN is_canceled = 1 THEN 1 ELSE 0 END) AS total_cancellation,
+	CONCAT(CAST(SUM(CASE WHEN is_canceled = 1 THEN 1 ELSE 0 END) * 100.0 / COUNT(*) AS decimal(5,2)),'%') AS cancellation_rate
+	FROM hotel_booking
+	GROUP BY FORMAT(reservation_status_date,'yyyy') ;
+
+--(7) MONTH WISE BOOKING CANCELLATION ANALYSIS
+
+	SELECT
+	MONTH(reservation_status_date) AS month_num,
+	FORMAT(reservation_status_date,'MMM') AS month,
+	COUNT(*) AS total_booking,
+	SUM(CASE WHEN is_canceled = 1 THEN 1 ELSE 0 END) AS total_cancellation,
+	CONCAT(CAST(SUM(CASE WHEN is_canceled = 1 THEN 1 ELSE 0 END) * 100.0 / COUNT(*) AS decimal(5,2)),'%') AS cancellation_rate
+	FROM hotel_booking
+	GROUP BY MONTH(reservation_status_date), FORMAT(reservation_status_date,'MMM') 
+	ORDER BY MONTH(reservation_status_date) ASC ;
+	
